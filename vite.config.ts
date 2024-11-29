5c0001d48c6e25f84bcd4a867f0e0a631bbcc79c
peeranat-dan <ppeeranat.d@gmail.com>
2024-11-29 08:20:11 +0700

    style: prettier format

--- vite.config.ts
+++ vite.config.ts
@@ -6,30 +6,30 @@ import tailwindcss from 'tailwindcss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		lib: {
-			entry: resolve(__dirname, './lib/index.ts'),
-			name: 'react-beautiful-timeline',
-			fileName: (format) => `index.${format}.js`,
-		},
-		rollupOptions: {
-			external: ['react', 'react-dom', 'tailwindcss'],
-			output: {
-				globals: {
-					react: 'React',
-					'react-dom': 'ReactDOM',
-					tailwindcss: 'tailwindcss',
-				},
-			},
-		},
-		sourcemap: true,
-		emptyOutDir: true,
-		copyPublicDir: false,
-	},
-	plugins: [react(), dts({ rollupTypes: true })],
-	css: {
-		postcss: {
-			plugins: [tailwindcss],
-		},
-	},
+  build: {
+    lib: {
+      entry: resolve(__dirname, './lib/index.ts'),
+      name: 'react-beautiful-timeline',
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom', 'tailwindcss'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+          tailwindcss: 'tailwindcss',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+    copyPublicDir: false,
+  },
+  plugins: [react(), dts({ rollupTypes: true })],
+  css: {
+    postcss: {
+      plugins: [tailwindcss],
+    },
+  },
 });
